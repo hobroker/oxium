4b2620805dfc65b8af69f73af76f70a86c0a6ad7
hobroker <igor.leahu24@gmail.com>
2020-02-07 16:11:22 +0200

    refactoring

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,11 +1,9 @@
-const PKG_NAME = 'oxium';
+export const PKG_NAME = 'oxium';
 
-const ID = 'id';
-const HANDLER = 'handler';
-const IS_LOADED = 'isLoaded';
-const FEATURES = 'features';
-const META = '_';
+export const ID = 'id';
+export const HANDLER = 'handler';
+export const IS_LOADED = 'isLoaded';
+export const FEATURES = 'features';
+export const META = '_';
 
-export { PKG_NAME };
-
-export { ID, FEATURES, META, HANDLER, IS_LOADED };
+export const HANDLER_NOT_READY_RESULT = false;
